1a594e17d586a8bcc413b276d9f4d24a978cc504
Traize <bishopdc@yandex.ru>
2023-10-16 16:42:54 +0300

    comments

--- 10.js
+++ 10.js
@@ -5,15 +5,15 @@ const jeyson = '{"name":"John","age":25,"currency":["rub","usd"],"obj":{"asd":12
 // как и в eval можно вставить скрипт
 const jsonObject = (new Function("return " + jeyson))()
 
-console.log(JSON.parse(jeyson))
+
 console.log(jsonObject)
 
 
 
 // Learn javascript
 // Функции, объявленные через new Function, имеют [[Environment]],
-//  ссылающийся на глобальное лексическое окружение, а не на родительское.
-//   Поэтому они не могут использовать внешние локальные переменные.
+// ссылающийся на глобальное лексическое окружение, а не на родительское.
+// Поэтому они не могут использовать внешние локальные переменные.
 function parse() {
     const jeyson2 = '{"name":"John","age":25,"currency":["rub","usd"],"obj":{"asd":123},"bool":true,"isNUll":null}'
     const jsonObject = (new Function("return " + jeyson2))
